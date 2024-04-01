c71120803d3b85ed388790e5218cc9b0f0a8f0af
Thiago Lopes <thiagoslopes8@outlook.com>
2024-04-01 16:24:22 -0300

    Added project colors

---
+++ src/theme/colors.ts
@@ -0,0 +1,15 @@
+export const colors = {
+  primaryColor: '#1DA1F2',
+  darkTxtColor: '#14171A',
+  greyColor: '#657786',
+  lightGreyColor: '#AAB8C2',
+  extraLightGreyColor: '#F5F8FA',
+  lightestGrayColor: '#E1E8ED',
+  lessDarkColor: '#1B1F23',
+  likeColor: '#F62357',
+  retweetColor: '#30E830',
+  commentColor: '#30ABE8',
+  blackColor: '#000000',
+  whiteColor: '#FFFFFF',
+  backgroundBlurColor: '#00000040'
+}
